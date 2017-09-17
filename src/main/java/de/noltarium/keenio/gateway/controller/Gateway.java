bcd991311e7a54546739563a6e20a4f1bde5142b
nolte <nolte@ingo.de>
2017-09-17 12:45:24 +0200

    switch acceped content type to url

--- src/main/java/de/noltarium/keenio/gateway/controller/Gateway.java
+++ src/main/java/de/noltarium/keenio/gateway/controller/Gateway.java
@@ -1,5 +1,10 @@
 package de.noltarium.keenio.gateway.controller;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.util.MultiValueMap;
@@ -7,6 +12,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import de.noltarium.keenio.gateway.services.AnalyticsStorage;
 import lombok.extern.slf4j.Slf4j;
@@ -19,10 +28,19 @@ public class Gateway {
 	@Autowired
 	private AnalyticsStorage analyticsStorage;
 
-	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = {
-			MediaType.APPLICATION_ATOM_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
-	public void receiveTravisCINotification(MultiValueMap  analisticData) {
-		analyticsStorage.pushData(analisticData);
+	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	public void receiveTravisCINotification(@RequestBody MultiValueMap<String, Object> analisticData) throws JsonParseException, JsonMappingException, IOException {
+		log.debug("receive new travis ci notification");
+
+		// get the travis ci payload from requst
+		List<Object> payload = analisticData.get("payload");
+
+		//convert the json payload to a generic hash map
+		String object = (String) payload.get(0);
+		HashMap<String, Object> result = new ObjectMapper().readValue(object, HashMap.class);
+
+		log.debug("convert payload");
+		analyticsStorage.pushData(result);
 	}
 
 }
